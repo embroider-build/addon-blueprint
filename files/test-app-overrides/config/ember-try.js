746edfc25dc823f7f90ca79e9150799e90a8b1eb
Sergey Astapov <SergeAstapov@gmail.com>
2022-11-08 23:51:18 -0500

    add pnpm condition to yarn/npm branches in blueprint files

--- files/test-app-overrides/config/ember-try.js
+++ files/test-app-overrides/config/ember-try.js
@@ -6,6 +6,7 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 module.exports = async function () {
   return {
     <% if (yarn) { %>useYarn: true,
+    <% } else if (pnpm) { %>usePnpm: true,
     <% } %>scenarios: [
       {
         name: 'ember-lts-3.28',
