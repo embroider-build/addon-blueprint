b1efe1001cf0eabe8daccbd346f0945209901d8e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-03 11:33:46 -0400

    Update typescript test

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -139,6 +139,7 @@ describe('ember addon <the addon> -b <this blueprint>', () => {
     let cwd = '';
     let tmpDir = '';
     let distDir = '';
+    let distTypesDir = '';
 
     beforeAll(async () => {
       tmpDir = await createTmp();
@@ -158,6 +159,7 @@ describe('ember addon <the addon> -b <this blueprint>', () => {
 
       cwd = path.join(tmpDir, name);
       distDir = path.join(cwd, name, 'dist');
+      distTypesDir = path.join(cwd, name, 'dist-types');
 
       // Remove because ember-cli ignores --skip-npm.
       // At present, ember-cli installs `ember-cli-typescript`, which then
@@ -190,13 +192,18 @@ describe('ember addon <the addon> -b <this blueprint>', () => {
       let contents = await dirContents(distDir);
 
       expect(contents).to.deep.equal([
-        'index.d.ts',
-        'index.d.ts.map',
         'index.js',
         'index.js.map',
-        'template-registry.d.ts',
         'template-registry.js',
         'template-registry.js.map',
+      ]);
+
+      let typesContents = await dirContents(distTypesDir);
+
+      expect(typesContents).to.deep.equal([
+        'index.d.ts',
+        'index.d.ts.map',
+        'template-registry.d.ts',
       ]);
     });
 
