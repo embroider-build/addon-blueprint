4c583043d5bcf09ea82b79db165ddcbebdfb638b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-03 13:07:36 -0400

    Looks like ember-cli doesn't yet have Simon's TS changes

--- files/__addonLocation__/unpublished-development-types/index.d.ts
+++ files/__addonLocation__/unpublished-development-types/index.d.ts
@@ -2,7 +2,6 @@
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
 
 import 'ember-source/types';
-import "ember-source/types/preview";
 
 import '@glint/environment-ember-loose';
 
