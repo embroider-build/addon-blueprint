306b61fd2e7b38e56ca424b106052eafa1193332
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-17 07:56:03 -0400

    Remove node resolve

--- files/__addonLocation__/rollup.config.mjs
+++ files/__addonLocation__/rollup.config.mjs
@@ -1,5 +1,4 @@
 import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 <% if (!isExistingMonorepo) { %>import copy from 'rollup-plugin-copy';
 <% } %>import { Addon } from '@embroider/addon-dev/rollup';
 
@@ -41,9 +40,6 @@ export default {
       extensions,
       babelHelpers: 'bundled',
     }),
-
-    // Allows rollup to resolve imports of files with the specified extensions
-    nodeResolve({ extensions }),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
