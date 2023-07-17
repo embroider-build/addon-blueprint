779806326fff0fc12523ed48f2ccd58664899627
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-17 09:17:38 -0400

    Add fixtures

---
+++ tests/fixtures/default/test-app/tests/rendering/co-located-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+
+module('Rendering | co-located', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`<CoLocated />`);
+
+    assert.dom().hasText('Hello, from a co-located component');
+  })
+});
---
+++ tests/fixtures/default/test-app/tests/rendering/template-only-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+
+module('Rendering | template-only', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`<TemplateOnly />`);
+
+    assert.dom().hasText('Hello from a template-only component');
+  })
+});
