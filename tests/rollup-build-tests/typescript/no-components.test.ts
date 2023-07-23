ebeb9138790901045f7ad94b501b8d4af18a57cb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-23 14:58:09 -0400

    Create failing test for the situation which needs rollup/plugin-resolve

---
+++ tests/rollup-build-tests/typescript/no-components.test.ts
@@ -0,0 +1,57 @@
+import path from 'node:path';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { AddonHelper, dirContents } from '../../helpers.js';
+
+describe(`rollup-build | typescript-utils`, () => {
+  let distDir = '';
+  let declarationsDir = '';
+  let helper = new AddonHelper({
+    packageManager: 'pnpm',
+    args: ['--typescript'],
+    scenario: 'typescript-utils',
+  });
+
+  beforeAll(async () => {
+    await helper.setup();
+    await helper.installDeps();
+
+    distDir = path.join(helper.addonFolder, 'dist');
+    declarationsDir = path.join(helper.addonFolder, 'declarations');
+  });
+
+  afterAll(async () => {
+    await helper.clean();
+  });
+
+  it('builds with success', async () => {
+    // Copy over fixtures
+    await helper.fixtures.useAll();
+
+    let buildResult = await helper.build();
+
+    expect(buildResult.exitCode).toEqual(0);
+
+    let distContents = (await dirContents(distDir)).filter(
+      // these files have a hash that changes based on file contents
+      (distFile) => !distFile.startsWith('_rollupPluginBabelHelpers')
+    );
+    let declarationsContents = await dirContents(declarationsDir);
+
+    expect(distContents).to.deep.equal([
+      '_app_',
+      'components',
+      'index.js',
+      'index.js.map',
+      'template-registry.js',
+      'template-registry.js.map',
+    ]);
+
+    expect(declarationsContents).to.deep.equal([
+      'index.d.ts',
+      'index.d.ts.map',
+      'template-registry.d.ts',
+      'template-registry.d.ts.map',
+    ]);
+  });
+});
