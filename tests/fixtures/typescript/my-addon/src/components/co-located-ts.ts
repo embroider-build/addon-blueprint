6865bd018fbaec209fa3c74826e3cef94a67709e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-27 13:16:03 -0400

    Add service

--- tests/fixtures/typescript/my-addon/src/components/co-located-ts.ts
+++ tests/fixtures/typescript/my-addon/src/components/co-located-ts.ts
@@ -1,5 +1,9 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import Example from '../services/example';
 
 export default class CoLocatedTs extends Component {
+  @service declare example: Example;
+
   whereAmI = 'from a co-located TS component';
 }
---
+++ tests/fixtures/typescript/my-addon/src/services/example.ts
@@ -0,0 +1,5 @@
+import Service from '@ember/service';
+
+export default class Example extends Service {
+  foo = 'hello from service';
+}
