c7cc40ce4f9b5c6ca2c1ba73a0a7a569ae70adf0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-07 11:33:12 -0500

    lints

--- tests/fixtures/explicit-imports/test-app/types/registry.d.ts
+++ tests/fixtures/explicit-imports/test-app/types/registry.d.ts
@@ -4,6 +4,6 @@ import '@glint/environment-ember-template-imports';
 import { default as MyAddonRegistry } from 'my-addon/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export default interface Registry extends MyAddonRegistry {}
 }
